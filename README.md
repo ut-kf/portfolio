77ff13ea7bbcc35cdf7085a1a432aa65e96e7dc7
ut-kf <astervnir000505@gmail.com>
2025-09-15 11:57:50 +0900

    jsとREADME追加

---
+++ README.md
@@ -0,0 +1,8 @@
+# ポートフォリオ
+私のポートフォリオ
+
+# 使用技術
+- HTML
+- CSS
+- JavaScript
+- GitHub Pages
---
+++ script.js
@@ -0,0 +1,4 @@
+// ページの読み込み時にコンソールにメッセージを表示
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("script.jsが正しく読み込まれました！");
+})
