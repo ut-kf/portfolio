e39bfeb2cfcf1f645c230bda3258f995ce5678dc
ut-kf <astervnir000505@gmail.com>
2025-09-25 00:06:21 +0900

    htmlとjsの修正

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // ページの読み込み時にコンソールにメッセージを表示
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("script.jsが正しく読み込まれました！");
+  console.log("script.jsが正しく読み込まれました！");
 })
+
+// ボタンをクリックしたときにメッセージを表示
+const showMessage = () => {
+  document.getElementById("message").textContent = "こんにちは！JavaScriptが動いています🎉";
+}
